0bf00cd63c2f6c92f9f2d414fb8b601b8cd6ed33
Kobra3390 <angelonunziolombardi@gmail.com>
2021-06-19 16:04:34 +0200

    aggiunta la funzione Encrypt_File

--- Encry.py
+++ Encry.py
@@ -4,6 +4,7 @@ from main_function import *
 
 console = Console()
 
+# ascii art
 console.print("""[yellow]
 ▓█████  ███▄    █  ▄████▄   ██▀███ ▓██   ██▓
 ▓█   ▀  ██ ▀█   █ ▒██▀ ▀█  ▓██ ▒ ██▒▒██  ██▒
@@ -18,9 +19,12 @@ console.print("""[yellow]
 """)
 
 console.print("[blue]1 - Generate a Key")
+console.print("[blue]2 - Encrypt the File")
 print("\n")
 
 options = int(input("Enter an options --> "))
 
 if(options == 1):
-   Generate_Key()
+  Generate_Key()
+if(options == 2):
+  Encrypt_File()
--- main_function.py
+++ main_function.py
@@ -1,10 +1,30 @@
 from cryptography.fernet import Fernet
 
+
+# Function to Generate the key
 def Generate_Key():
    # key generation
    key = Fernet.generate_key()
    # string the key in a file
    with open('filekey.key', 'wb') as filekey:
       filekey.write(key)
+
+# Function to Encrypt the file using the key generated
+def Encrypt_File():
+   # openning the key
+   with open('filekey.key', 'rb') as filekey:
+      key = filekey.read()
+   # using the generated key
+   fernet = Fernet(key)
+   # opening the original file to encrypt
+   file_name = str(input("Enter a file name to encrypt: "))
+   with open(file_name, 'rb') as file:
+      original = file.read()
+   # emcrypting the file
+   encrypted = fernet.encrypt(original)
+   # openint the file in write mode and 
+   # writing the encryèted data
+   with open(file_name, 'wb') as encrypted_file:
+      encrypted_file.write(encrypted)
 
       
